779587c0a24c85ba65d44430f88178d20137caa9
Vikrant Gawde <vikrant.gawde10@gmail.com>
2020-08-14 12:03:52 -0700

    Printing using main function

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print("Hello world !")
-print("My name is is Nihar ")
-print("this is just hte starting");
+# print("Hello world !")
+# print("My name is is Nihar ")
+# print("this is just the starting");  # semicolon not required in python - you will fall in love with how easy it is
+
+# Here's another way of doing the same thing as above using Main function
+
+
+# defining main function
+def main():
+    print("Hello world !")
+    print("My name is is Nihar ")
+    print("this is just the start")
+
+if __name__ == "__main__":
+    main() # calling above main function
