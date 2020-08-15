2761b850ca4e2c3eb09471c0fbb5499f8a2ac64d
Vikrant Gawde <vikrant.gawde10@gmail.com>
2020-08-15 07:53:22 -0700

    vik.py has been modified to reflect latest changes

--- Vikrant/vik.py
+++ Vikrant/vik.py
@@ -1,2 +1,28 @@
  # Testing Git folder
- 
+
+
+
+
+
+# Trying Classes and Objects
+class Page:
+
+    def __init__(self,length,type):
+        self.length = length
+        self.type = type
+
+
+class Tab(class Page):
+
+    def __init__(self,title, is_current, page, class_page):
+        self.title = title
+        self.is_current = is_current
+        self.page = page
+        self.length = class_page.length
+        self.type = class_page.type
+
+    def close():
+        return "{} is currently {}".format(self.title, self.is_current)
+
+    def reload():
+        return "{} is currently reloading".format(self.page)
