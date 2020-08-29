d15470fd317666ae5d44f6278d1e9858711aefdf
Vikrant Gawde <vikrant.gawde10@gmail.com>
2020-08-29 12:27:22 -0700

    good binary tree stuff

--- Vikrant/python_practice.py
+++ Vikrant/python_practice.py
@@ -417,9 +417,9 @@
 # for q in queries:
 #     print(queries[(q[1]^0)%1])
 
-arr = [0,0,0,0,0]
-p,q,k,i,j = arr
-print(p)
+# arr = [0,0,0,0,0]
+# p,q,k,i,j = arr
+# print(p)
 
 # import numpy as np
 # empty_list = [[0,0,0,0,0],
@@ -457,7 +457,7 @@ print(p)
 
 
 
-Best ever array manipulation
+# Best ever array manipulation
 
 
 # def arrayManipulation(n, queries):
@@ -476,3 +476,156 @@ Best ever array manipulation
 
 # A tree whose nodes have atmost 2 elements is called a binary tree.
 # left and right child
+
+
+
+
+# @classmethod vs @staticmethod
+
+# class MyClass:
+#     def method(self):
+#         return 'instance method called', self
+#
+#     @classmethod
+#     def classmethod(cls):
+#         return 'class method called', cls
+#
+#     @staticmethod
+#     def staticmethod():
+#         return 'static method called'
+#
+# class Pizza:
+#     def __init__(self, ingredients, radius):
+#         self.ingredients = ingredients
+#         self.radius = radius
+#
+#     def __repr__(self):
+#         return f'Pizza({self.ingredients})'
+#
+#     def area(self):
+#         return self.__circle_area(self.radius)
+#
+#     @staticmethod
+#     def __circle_area(r):
+#         return r**2*math.pi
+#
+#
+#     @classmethod
+#     def margherita(cls):
+#         return cls(['cheese','tomatoes'])
+#
+#     @classmethod
+#     def prosciutto(cls):
+#         return cls(['cheese','tomatoes','ham','mushrooms'])
+
+
+
+# print(Pizza.margherita())
+# print(Pizza.prosciutto())
+
+# obj = MyClass()
+# print(obj.method())
+
+# print('='*50)
+# # print(obj.classmethod())
+# # print(obj.staticmethod())
+#
+#
+#
+# print(MyClass.classmethod())
+# print(MyClass.staticmethod())
+# print(MyClass.method())
+#
+#
+#
+#
+
+
+
+
+# Recursion Basics Towers of Hanoi - minimum 2^n - 1 steps
+
+# so for 3 disks minimum 7 steps
+#
+# def Hanoi(disk, source, dest, aux):
+#     if disk == 1:
+#         print("Move disk 1 from source", source, "to destination", destination)
+#         return
+#     Hanoi(disk-1, source, auxiliary, destination)
+#     print("Move disk", n, "from source", soure, "to destination", destination)
+#     Hanoi(disk-1, auxiliary, destination, source)
+#
+#
+#
+#
+# # Driver code
+# disk = 4
+# Hanoi(disk,'A','B','C')
+# A, C, B are the name of rods
+
+# Contributed By Dilip Jain
+
+
+
+
+# Binary Tree in Python
+class Node:
+
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.data = data
+
+    # Insert node
+    def insert(self, data):
+
+        if self.data:
+            if data < self.data:
+
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+
+        else:
+            self.data = data
+
+
+        # Print tree
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print( self.data),
+        if self.right:
+            self.right.PrintTree()
+
+        # Inorder Traversal
+    def inorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.inorderTraversal(root.left)
+            res.append(root.data)
+            res = res+ self.inorderTraversal(root.right)
+        return res
+
+
+root = Node(27)
+
+root.insert(14)
+root.insert(35)
+root.insert(10)
+root.insert(19)
+root.insert(31)
+root.insert(42)
+
+
+
+print(root.PrintTree(),sep='=')
+print(root.inorderTraversal(root),sep='=')
