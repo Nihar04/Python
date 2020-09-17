d24bb79b9dfb9dc883873e1756e8897f2165595a
Nihar04 <69451996+Nihar04@users.noreply.github.com>
2020-09-17 09:47:01 +0530

    Addition of if statements and def.

--- main.py
+++ main.py
@@ -34,6 +34,20 @@ else:
     print("You are neither male nor tall")
 
     
+     # Def, Return and If statements
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+
+print(max_num(30, 40, 5))
+
+    
  #   THE GUESSING GAME
 secert_word = "giraffe"
 guess = ""
